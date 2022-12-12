8645dc15fe1c68c1e51a08b776412357c78f0b74
Z4NR34L <software@zanreal.net>
2022-12-12 09:55:42 +0100

    Updated gitignore and build script

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
 echo "1/3 Building frontend..."
-cd client
+cd client || exit
 npm run build
 rm build/index.html
 echo "2/3 Creating api folder..."
-cd ../server
+cd ../server || exit
 rm config.php
 touch config.php
 mkdir files2
 cp files/.htaccess files2
 rm -rf files/
-cp files2 files
+cp -r files2 files
 cd ..
 mkdir api
 cp server/index.php api
 cp server/.htaccess api
 cp server/composer.json api
 cp server/composer.lock api
-cp server/controllers api
-cp server/data api
-cp server/libs api
-cp server/models api
-cp server/vendor api
-cp server/files api
+cp -r server/controllers api
+cp -r server/data api
+cp -r server/libs api
+cp -r server/models api
+cp -r server/vendor api
+cp -r server/files api
 cp server/config.php api
 chmod -R 755 .
 cp client/src/index.php client/build
 echo "3/3 Generating zip..."
-cd client/build
+cd client/build || exit
 zip opensupports_dev.zip index.php
 zip -u opensupports_dev.zip .htaccess
 zip -u opensupports_dev.zip bundle.js
